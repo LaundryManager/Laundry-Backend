d01fe67cdf702478904fe62b2dc0066641db2587
João <dev.junqueira@gmail.com>
2023-01-22 22:48:17 -0300

    Changed hash type from hash_raw to hash_encoded so now we validate all the encoded hash for the password, not only the raw password. added .vscode to gitignore

--- src/tools/hash_password.rs
+++ src/tools/hash_password.rs
@@ -1,9 +1,7 @@
 use argon2::{self, Config};
 
 pub fn hash_password(password: String) -> String {
-    // -- Put salt as a env variable
     let salt = "salt_for_laundry_app".as_bytes();
     let config = Config::default();
-    let password = argon2::hash_raw(password.as_bytes(), salt, &config).unwrap();
-    password.iter().map(|x| format!("{:02x}", x)).collect()
+    argon2::hash_encoded(password.as_bytes(), salt, &config).unwrap()
 }
